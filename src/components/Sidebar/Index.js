b337291c89ad887ce330980ce410b41db3381dd5
Adriano Santtos <adrianosantosx2@hotmail.com>
2020-11-15 19:19:39 -0300

    de novo

---
+++ src/components/Sidebar/Index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { MdSettings, MdHome, MdHelp, MdSchool, MdMonetizationOn, MdInsertChart } from 'react-icons/md';
+
+import './style.css'
+
+export default function Dashboard(){
+    return(
+        <div className="container-dashboard">
+            <div className="card-dashboard">
+                <div className="sidebar-dashboard">
+                    <ul>
+                        <li><MdHome size={25} color="#ffffff" /><Link to="/login" className="link-dashboard">Home</Link></li>
+                        <li><MdSchool size={25} color="#ffffff" /><Link className="link-dashboard"></Link>Aprenda +</li>
+                        <li><MdMonetizationOn size={20} color="#ffffff" /><Link className="link-dashboard"></Link>Carteira</li>
+                        <li><MdInsertChart size={20} color="#ffffff" /><Link className="link-dashboard"></Link>Investimento</li>
+                        <li><MdSettings size={20} color="#ffffff" /><Link className="link-dashboard"></Link>Configurações</li>
+                        <li><MdHelp size={20} color="#ffffff" /><Link className="link-dashboard"></Link>Ajuda</li>
+                    </ul>
+                </div>
+                <div className="content-dashboard">
+                    <div className="header-dashboard">Teste</div>
+                    <div className="content">Teste</div>
+                </div>
+            </div>
+        </div>
+    );
+}
